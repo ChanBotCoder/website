fa260b80aa0be84ae9350825bceb1e7bc9116395
Chandru <106470788+ChanBotCoder@users.noreply.github.com>
2023-02-26 15:13:20 +0000

    Adding dyanmic data and API

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Chennai,India',
+    'salary':'Rs. 1,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Enginner',
+    'location':'Chennai,India',
+    'salary':'Rs. 1,00,000'
+  },
+  {
+    'id':3,
+    'title':'Frontend Developer',
+    'location':'Chennai,India'
+  },
+  {
+    'id':4,
+    'title':'Data Scientist',
+    'location':'Chennai,India',
+    'salary':'Rs. 1,00,000'
+  }
+  
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs = JOBS)
+
+@app.route("/api/jobs")
+def list_jobs(): 
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
